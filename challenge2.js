7ef137eddbb8add6904630d676ad5d729ffec266
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-23 17:20:30 -0400

    Solve challenge 2

---
+++ challenge2.js
@@ -0,0 +1,13 @@
+//Overall goal: Print all words in lowercase letters
+// Input: An array of strings
+// Output: Strings
+// Create an array of strings
+var strArray = ["I", "am", "an", "arraY", "oF", "StriNgs"];
+    // Iterate through the array
+    for (let i = 0; i < strArray.length; i++) {
+    // Store each word in a varaible
+    word = strArray[i];
+    // Make each word lowercase
+    // Print each word
+    console.log(word.toLowerCase());
+    }
